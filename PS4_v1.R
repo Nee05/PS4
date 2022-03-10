702fa3b1fdfc869945c95f49e0daafee3471416b
Neeharika Kakunuri <kakunuri.neeharika06@gmail.com>
2022-03-10 14:09:33 +0000

    Latest V3

---
+++ PS4_v1.R
@@ -0,0 +1,9 @@
+install.packages(c("readxl","tidyverse", "ggplot2", "remotes"))
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(remotes)
+data<-data_ps4
+View(data)
+remotes::install_github('bquast/rddtools')
+

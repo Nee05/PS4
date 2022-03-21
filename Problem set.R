0e6c6514daeace6f775ba985c126db8c1c93a4fe
giacomoloco <98044893+giacomoloco@users.noreply.github.com>
2022-03-21 16:33:32 +0000

    Add files via upload
    Finalised code V1

--- Problem set.R
+++ Problem set.R
@@ -1,15 +1,15 @@
-install.packages(c("readxl","tidyverse", "ggplot2", "remotes", "stargazer", "foreign"))
-library(readxl)
-library(tidyverse)
-library(ggplot2)
-library(remotes)
-library(haven)
+rm(list = ls())
+
 library(foreign)
 library(stargazer)
 library(haven)
+library(rlang)
 library(tidyverse)
-install.packages("finalfit")
 library(finalfit)
+library(rdrobust)
+library(ggplot2)
+library(binsreg)
+library(rddtools)
 
 data = read_dta("data_ps4.dta")
 
@@ -22,7 +22,6 @@ summary(data)
 table(is.na(data$lwage1), is.na(data$nonemp)) #All nonemp NAs are also lwage1 NAs
 table(is.na(data$lwage1), is.na(data$lwage0)) #Not all lwage0 NAs are also lwage1 NAs
 
-
 #Observing if lwage1 NAs (the most numerous) are distributed uniformely across the dataset
 
 Diff_mean=mean(data[is.na(data$lwage1),]$lwage0, na.rm=T)-mean(data[!is.na(data$lwage1),]$lwage0, na.rm=T)
@@ -34,77 +33,77 @@ Var=sqrt(var(data[is.na(data$lwage1),]$lwage0, na.rm=T)+var(data[!is.na(data$lwa
 Diff_mean=mean(data[is.na(data$lwage1),]$age_month, na.rm=T)-mean(data[!is.na(data$lwage1),]$age_month, na.rm=T)
 Var=sqrt(var(data[is.na(data$lwage1),]$age_month, na.rm=T)+var(data[!is.na(data$lwage1),]$age_month, na.rm=T))
 2*(1-pnorm(abs(Diff_mean/Var)))
-#Mean of age_month in the part of population where we do not observe the lwage1 is not statistically different
-#from the part of the population where we observe it
+
+#Distribution of age_month in the sample of population where we do not observe the lwage1 is not statistically different
+#from the sample of the population where we observe it
 
 Diff_mean=mean(data[is.na(data$lwage1),]$nonemp, na.rm=T)-mean(data[!is.na(data$lwage1),]$nonemp, na.rm=T)
 Var=sqrt(var(data[is.na(data$lwage1),]$nonemp, na.rm=T)+var(data[!is.na(data$lwage1),]$nonemp, na.rm=T))
 2*(1-pnorm(abs(Diff_mean/Var)))
-#Mean of nonemp in the part of population where we do not observe the lwage1 is not statistically different
+
+#Distribution of nonemp in the part of population where we do not observe the lwage1 is not statistically different
 #from the part of the population where we observe it
 
 Diff_mean=mean(data[is.na(data$lwage1),]$jobfind, na.rm=T)-mean(data[!is.na(data$lwage1),]$jobfind, na.rm=T)
 Var=sqrt(var(data[is.na(data$lwage1),]$jobfind, na.rm=T)+var(data[!is.na(data$lwage1),]$jobfind, na.rm=T))
 2*(1-pnorm(abs(Diff_mean/Var)))
-#Mean of jobfind in the part of population where we do not observe the lwage1 is not statistically different
+
+#Distribution of jobfind in the part of population where we do not observe the lwage1 is not statistically different
 #from the part of the population where we observe it
 
-#We can drop NAs of lwage1 without problems
+#We can drop out NAs of lwage1 without excessive concerns
 
 data = subset(data, !is.na(data$lwage1))
 summary(data)
-remotes::install_github('bquast/rddtools')
 
-install.packages("rddtools")
-vignette('rddtools')
-View(data)
-#trial <- rdd_data(y=rdd_data$age, x=rdd_data$, cutpoint=0)
-data$binedage<-cut(data$age_month, seq(min(data$age_month),max(data$age_month), 4))
-View(data)
-A <- ggplot(data, aes(x = age_month)) +
-  geom_density()
-A
-A + geom_vline(aes(xintercept = mean(age_month)), 
-               color = "red", linetype = "dashed", size = 1)
-data$binedage<-cut(data$age_month, seq(min(data$age_month),max(data$age_month), 4))
-View(data)
-B <- data%>% 
-  group_by(binedage) %>% 
-  mutate(Mean_lwage0 = mean(lwage0, na.rm = T)) %>%
-  ggplot(aes(x=age_month, y = Mean_lwage0)) + 
-  geom_point(shape = 1, alpha = 0.5) + 
-  geom_vline(aes(xintercept = 480), color = "blue" , linetype = "dashed", size =1) 
-B 
-install.packages("rddensity")
-library(rddensity)
+##RDD diagnostics
 
-install.packages("rdrobust")
-library(rdrobust)
+Density <- ggplot(data, aes(x = age_month)) +
+            geom_point()+
+            geom_density() + 
+            geom_vline(aes(xintercept = 480), 
+                         color = "red", linetype = "dashed", size = 1)+
+            labs(x="Age (months)", y="Density", title="Distribution of age")+
+            theme_classic()
+Density
 
-a<- rdplot(data$lwage0, data$age_month, p = 2, c = (480), binselect = "es")
-summary(a)
-#without bin size B
-B<- ggplot(data, aes(x =age,
-                     y = lwage0)) +
-  geom_point(alpha = 0.4) +
-  stat_summary_bin(fun.y = "mean" , bins = 20,
-                   color = "yellow", size = 2, geom = "point")
-B
-B + geom_vline(aes(xintercept = mean(age)), 
-               color = "red", linetype = "dashed", size = 1)
+Prev_wage = rdplot(data$lwage0, data$age_month, c=480, p=2, nbins=60, col.dots = "black",
+       x.label = "Age (months)", y.label="Mean of previous wage (log)", title = "" )
 
-plot1<- rddensity(data$age_month, c = 480, p = 2, q = p+1,
-                  fitselect = "restricted", vce = "plugin", massPoints = TRUE,
-                  bino = TRUE)
+##RDD estimates graph
 
-#install.packages("binsreg")
-#library(binsreg)
+unemp_RDD <- rdplot(data$nonemp, data$age_month, c=480, p=2, nbins=60, col.dots = "black",
+                    x.label = "Age (months)", y.label="Unemployment duration", title = "" )
 
-#B plot 2
-#binscatter1<-binsreg(data$lwage0, data$age_month)
-#binscatter1$bins_plot + geom_vline (xintercept = 39, colour = "red" ) + xlab("Age in months") + ylab("Log of wages at layoff")
-#ggtitle("Plot of age vs wages at layoff") + 
-  #theme(plot.title = element_text(hjust = 0.3, vjust = 0.3, face = 'italic')) +
-  #theme(element_line(margin(t = 3, r = 3, b = 3, l = 3, unit = "pt")))
+lwage1_RDD <- rdplot(data$lwage1, data$age_month, c=480, p=2, nbins=60, col.dots = "black",
+                     x.label = "Age (months)", y.label="Mean of new wage (log)", title = "" )
 
+jobfind_RDD <- rdplot(data$jobfind, data$age_month, c=480, p=2, nbins=60, col.dots = "black",
+                     x.label = "Age (months)", y.label="Probability of finding a new job", title = "" )
+
+##Regression Table
+
+data$cutoff = ifelse(data$age_month>=480, 1, 0)
+
+reg1A<-lm(lwage1~cutoff, data = data)
+reg2A<-lm(lwage1~cutoff, data = data, subset = data$age>=35&data$age<=45)
+reg3A<-lm(lwage1~cutoff*age, data = data)
+reg4A<-lm(lwage1~cutoff*age+cutoff*I(age^2)+cutoff*I(age^3)+cutoff*I(age^4), data = data)
+reg5A<-rdrobust(data$lwage1, data$age, c=40, covs=data$lwage0)
+
+reg1B<-lm(jobfind~cutoff, data = data)
+reg2B<-lm(jobfind~cutoff, data = data, subset = data$age>=35&data$age<=45)
+reg3B<-lm(jobfind~cutoff*age, data = data)
+reg4B<-lm(jobfind~cutoff*age+cutoff*I(age^2)+cutoff*I(age^3)+cutoff*I(age^4), data = data)
+reg5B<-rdrobust(data$jobfind, data$age, c=40, covs=data$lwage0)
+
+reg1C<-lm(nonemp~cutoff, data = data)
+reg2C<-lm(nonemp~cutoff, data = data, subset = data$age>=35&data$age<=45)
+reg3C<-lm(nonemp~cutoff*age, data = data)
+reg4C<-lm(nonemp~cutoff*age+cutoff*I(age^2)+cutoff*I(age^3)+cutoff*I(age^4), data = data)
+reg5C<-rdrobust(data$nonemp, data$age, c=40, covs=data$lwage0)
+
+stargazer(reg1A, reg2A, reg3A, reg4A)
+stargazer(reg1B, reg2B, reg3B, reg4B)
+stargazer(reg1C, reg2C, reg3C, reg4C)
 
